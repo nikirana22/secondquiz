ec2a69dbac5ba67b3d74eb048b98c3a2d4de4036
ehtishaam <eqamar@lolofit.com>
2022-07-02 20:24:51 +0530

    refactor(quiz_time): minor compilation error fix

--- lib/widget/answers.dart
+++ lib/widget/answers.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:quiz/provider/timer_provider.dart';
+
 import '../provider/quiz_provider.dart';
 
 class Answers extends StatefulWidget {
-
   Answers({Key? key}) : super(key: key);
 
   @override
@@ -18,9 +18,8 @@ class _AnswersState extends State<Answers> {
   @override
   Widget build(BuildContext context) {
     print('check if this is calling on every tick--------------------------');
-    QuizProvider quizProvider =
-        Provider.of<QuizProvider>(context);
-    timerProvider = Provider.of<TimerProvider>(context,listen: false);
+    QuizProvider quizProvider = Provider.of<QuizProvider>(context);
+    timerProvider = Provider.of<TimerProvider>(context, listen: false);
     Size size = MediaQuery.of(context).size;
     double height = size.height;
     double width = size.width;
@@ -29,8 +28,9 @@ class _AnswersState extends State<Answers> {
       width: width,
       height: height * 0.35,
       child: ListView.builder(
-          itemCount:
-              quizProvider.list[quizProvider.questionIndex]["answer"].length,
+          itemCount: (quizProvider.list[quizProvider.questionIndex]["answer"]
+                  as List<String>)
+              .length,
           itemBuilder: (context, index) {
             return Column(
               children: [
@@ -41,7 +41,8 @@ class _AnswersState extends State<Answers> {
                   shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(25)),
                   tileColor: changeTileColorOnButtonClick(
-                      quizProvider.isCorrectAnswer(index),index /*will allso need listview indes if we need to only change the selected btn*/) /*quizProvider.colorList[index]*/,
+                      quizProvider.isCorrectAnswer(index),
+                      index /*will allso need listview indes if we need to only change the selected btn*/) /*quizProvider.colorList[index]*/,
                   trailing: _selectedValue != null &&
                           quizProvider.isCorrectAnswer(index)
                       ? const Icon(Icons.check)
@@ -49,22 +50,20 @@ class _AnswersState extends State<Answers> {
                   /*quizProvider.iconList[index]*/
                   /*enabled: quizProvider.clickable,*/
                   title: Text(
-                    quizProvider.list[quizProvider.questionIndex]["answer"]
-                        [index],
+                    (quizProvider.list[quizProvider.questionIndex]["answer"]
+                        as List<String>)[index],
                     textAlign: TextAlign.center,
                     style: const TextStyle(
                         color: Colors.blue,
                         fontWeight: FontWeight.bold,
                         fontSize: 20),
                   ),
-                  onTap:
-                  _selectedValue == null
-                      ?
-                  () {
-                    print('index $index');
+                  onTap: _selectedValue == null
+                      ? () {
+                          print('index $index');
                           _selectedValue = index;
                           quizProvider.optionSelected(timerProvider!);
-                    setState((){});
+                          setState(() {});
                         }
                       : null,
                 ),
@@ -76,7 +75,7 @@ class _AnswersState extends State<Answers> {
   }
 
   //refactor to a better name
-  Color changeTileColorOnButtonClick(bool isCorrectAnswer,int index) {
+  Color changeTileColorOnButtonClick(bool isCorrectAnswer, int index) {
     Color tileColor = Colors.white;
     /*if (_selectedValue == null) {
       tileColor = Colors.white;
@@ -90,6 +89,6 @@ class _AnswersState extends State<Answers> {
     }
     // _selectedValue=-1;
     // _selectedValue=null;
-      return tileColor;
+    return tileColor;
   }
 }
